67f323137074650b74a1a08a472a3d5904b418ba
Angel Barrientos <angel.barrientos@pagofacil.net>
2019-12-11 10:09:28 -0600

    new basic asserts are added

--- Test/Unit/Source/Http/ClientTest.php
+++ Test/Unit/Source/Http/ClientTest.php
@@ -41,31 +41,18 @@ class ClientTest extends TestCase
      */
     public function request(): void
     {
-        $request = new PrimitiveRequest(
-            ClientInterface::POST,
-            $this->requestData()
-        );
-    }
-
-    /**
-     * @test
-     */
-    public function ClientMockup(): void
-    {
-        $response = new Response(
-            json_encode($this->responseData()),
-            200
-        );
-
+        $match = null;
         $request = new PrimitiveRequest(
             ClientInterface::POST,
             $this->requestData()
         );
 
-        $client = $this->getMockBuilder(PagoFacil::class)
-            ->setConstructorArgs(['https://demo.url'])
-            ->setMethods(['sendRequest'])
-            ->getMock();
-        $client->expects($this->once())->with($request)->willReturn($response);
+        preg_match('/=transaccion/', $request->getBody(), $match);
+        $this->assertGreaterThanOrEqual(1, count($match));
+
+        preg_match(
+            '/\[monto\]\=([0-9]*[.]?[0-9]+)/', $request->getBody(), $match
+        );
+        $this->assertGreaterThanOrEqual(1, count($match));
     }
 }
