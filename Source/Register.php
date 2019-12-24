e8c6deac50c3de8429f458ea9e318a7bc50af75e
Angel Barrientos <angel.barrientos@pagofacil.net>
2019-12-24 13:42:18 -0600

    The Register pattern are created

---
+++ Source/Register.php
@@ -0,0 +1,103 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PagoFacil\Payment\Source;
+
+use ArrayObject;
+use Exception;
+
+final class Register
+{
+    private static $instance;
+    /** @var ArrayObject $data */
+    private $data;
+
+    private function __construct()
+    {
+        $this->data = new ArrayObject();
+    }
+
+    static public function getInstance(): Register
+    {
+        if (!static::$instance instanceof Register) {
+            static::$instance = new static();
+        }
+
+        return static::$instance;
+    }
+
+    /**
+     * @param string $key
+     * @return mixed
+     * @throws Exception
+     */
+    public function get(string $key)
+    {
+        if (!$this->data->offsetExists($key)) {
+            throw new Exception('Key not exists');
+        }
+
+        return $this->data->offsetGet($key);
+    }
+
+    /**
+     * @param string $key
+     * @param $value
+     * @throws Exception
+     */
+    public function set(string $key, $value): void
+    {
+        if (!$this->data->offsetExists($key)) {
+            throw new Exception('The key are really exists');
+        }
+
+        $this->data->offsetSet($key, $value);
+    }
+
+    /**
+     * @param string $key
+     * @throws Exception
+     */
+    public function deleteKey(string $key): void
+    {
+        if (!$this->data->offsetExists($key)) {
+            throw new Exception('Key not exists');
+        }
+
+        $this->data->offsetUnset($key);
+    }
+
+    private function __wakeup()
+    {
+    }
+
+    private function __clone()
+    {
+    }
+
+    public function __destruct()
+    {
+        $this->data = null;
+    }
+
+    /**
+     * @param string $key
+     * @param $value
+     * @throws Exception
+     */
+    static public function add(string $key, $value): void
+    {
+        static::getInstance()->set($key, $value);
+    }
+
+    /**
+     * @param string $key
+     * @return mixed
+     * @throws Exception
+     */
+    static public function bringOut(string $key)
+    {
+        return static::getInstance()->get($key);
+    }
+}
