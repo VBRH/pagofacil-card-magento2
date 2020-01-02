fe63394f7b576b924a86cdffc63dc911adc0658f
Angel Barrientos <angel.barrientos@pagofacil.net>
2020-01-02 16:33:19 -0600

    First part.

--- view/frontend/web/js/view/payment/method-renderer/cash-form.js
+++ view/frontend/web/js/view/payment/method-renderer/cash-form.js
@@ -1,28 +1,19 @@
 define(
     ['Magento_Checkout/js/view/payment/default'],
     function (Component, $) {
-            'use strict';
-            return Component.extend(
-                {
-                    defaults: {
-                        template: 'Pagofacil_Card/payment/cash-form'
-                    },
-                    getCode: function () {
-                        return 'pagofacil_cash';
-                    },
-                    isActive: function () {
-                        return true;
-                    },
-                    getData: function () {
-                        return {
-                            'method': this.getCode(),
-                            'additional_data': {}
-                        }
-                    },
-                    getMunicipality: function () {
-                        return document.querySelector('#pf-municipality').value;
-                    },
+        'use strict';
+        return Component.extend(
+            {
+                defaults: {
+                    template: 'Pagofacil_Card/payment/cash-form'
+                },
+                getCode: function () {
+                    return 'pagofacil_cash';
+                },
+                isActive: function () {
+                    return true;
                 }
-            );
+            }
+        );
     }
 );
